79af94e7a02cfb78df485a77c60ab16da6c3da69
SHANU\velsh <velshan49@gmail.com>
2023-10-12 15:56:07 +0000

    commit for initial code

---
+++ alkeon-app/src/test/java/com/born/alkeon/AlkeonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.born.alkeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkeonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
